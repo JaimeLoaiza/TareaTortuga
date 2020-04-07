10fad6e30d1d0c20c1674a8c4f6069be66058529
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-04-07 16:19:08 -0500

    Tarea-TortugaList

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTortuga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tortugas tortuga = new Tortugas();
+            Console.WriteLine("Digite el Nombre de la Tortuga");
+            tortuga.SetNombre(Console.ReadLine());
+            Console.WriteLine("Digite la Especie de la Tortuga");
+            tortuga.Especie = Console.ReadLine();
+            Console.WriteLine("Digite la Clasificación de la tortuga");
+            tortuga.Clasificacion = Console.ReadLine();
+            Console.WriteLine("La Información Suministrada fue:");
+            Console.WriteLine("Nombre: {0}", tortuga.GetNombre());
+            Console.WriteLine("Especie: {0}", tortuga.Especie);
+            Console.WriteLine("Clasificación: {0}", tortuga.Clasificacion);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tortugas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTortuga
+{
+    public class Tortugas
+    {
+        private string nombre;
+        private string especie;
+        private string clasificacion;
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+        public void SetNombre(string Nombre)
+        {
+            this.nombre = Nombre;
+        }
+        public string Especie
+        {
+            get
+            {
+                return this.especie;
+            }
+            set
+            {
+                this.especie = value;
+            }
+        }
+        public string Clasificacion
+        {
+            get
+            {
+                return this.clasificacion;
+            }
+            set
+            {
+                this.clasificacion = value;
+            }
+        }
+    }
+}
